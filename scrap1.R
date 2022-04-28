cb0234c381080e56e7c7cb0f9cff06e093ca9a19
loren <lorenzorossi001@gmail.com>
2022-04-28 15:08:58 +0200

    codek

--- scrap1.R
+++ scrap1.R
@@ -10,40 +10,85 @@ library(mvtnorm)
 library(matrixcalc)
 library(datasets)
 library(readr)
+library(Matrix)
+library(glmnet)
+library(boot)
+library(glasso)
+library(readr)
+library(ggplot2)
+library(ggthemes)
+library(ggcorrplot)
+library(dplyr)
+library(caret)
+library('ade4')
+library("paran")
+library("ROSE")
+library('pROC')
+library('reshape')
+library('class')
+library('tidyr')
+library('kernlab')
 
 ar <- read.csv('africa_recession.csv')
 vd <- read.csv('VariableDefinitions.csv')
 cb <- read.csv('company bankruptcy.csv')
 
-data <- na.omit(file)
+ar <- na.omit(ar)
 
 # Keep/drop columns (TYPE IN THE COL NO.)
 
-data <- data[-c(50)]
-summary(data)
+summary(ar)
 
-library('Matrix')
-library('glmnet')
-library('boot')
-grid = 10^seq(10,-2,length=100)
-lasso.mod = glmnet(x,y,alpha = 1,lambda = grid)
-summary(lasso.mod)
-plot(lasso.mod)
+#data visualisation
 
-set.seed(1)
-cv.out = cv.glmnet(x,y,alpha = 1)
-plot(cv.out)
-bestlam=cv.out$lambda.min
-lasso.pred=predict(lasso.mod,s=bestlam,newx=x)
-mean((lasso.pred-y)^2)
-out = glmnet(x,y,alpha=1,lambda=grid)
-lasso.coef = predict(out,type='coefficients',s=bestlam)
-lasso.coef
+barplot(table(ar$growthbucket),col=rainbow(2),
+        main="Frequency of Recession",
+        xlab="Recession Tag",
+        ylab="Number of Recession Events")
+box()
 
-select_s = which(lasso.coef>0 | lasso.coef<0)-1  # 去掉截距项
-select_s[select_s>0]
-data_x = re[select_s]          # 变量选择
-data_y = y
+ar$growthbucket<-as.factor(ar$growthbucket)
+summary(ar$growthbucket)
 
-nby <- gRapHD::neighbourhood(bF,orig=1001, rad=4)$v[,1]
-plot(bF,vert=nby, numIter=1000)
+#many hist
+
+#now we check the distribution of the independent variables
+#since we have about 50 independet variables we will partition the charts
+par(mfrow = c(10,5))
+i <- 1
+for (i in 1:50) 
+{
+  hist((ar[,i]), main = paste("Distibution of ", colnames(ar[i])), xlab = colnames(ar[i]))
+}
+
+recession_plots <- gather(ar,'feature','n',1:49)
+
+ggplot(recession_plots)+geom_density(aes(n))+ 
+  facet_wrap(~feature,scales='free')+labs(title= 'Density plot of predictors')+
+  theme_few()+ylab('Distribution of values')+ xlab('')
+
+#BN
+
+bn.hc <- hc(ar)
+am <- amat(bn.hc)
+
+library('ggm')
+plot(as(essentialGraph(am),'igraph'))
+
+
+
+
+#pm lasso
+C.body <- cov2cor(S.body)
+res.lasso <- glasso(C.body,rho=0.1)
+AM <- res.lasso$wi!=0 # etimate K matrix
+diag(AM) <-FALSE
+g.lasso <- as(AM,"graphNEL")
+nodes(g.lasso) <- names(gRbodyfat)
+glasso.body <- cmod(edgeList(g.lasso),data=gRbodyfat)
+plot(as(glasso.body,"igraph"))
+
+graph :: degree(as(glasso.body,"graphNEL"))
+
+
+
